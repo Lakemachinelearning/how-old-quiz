33824b80ba19bf06c28bafba98f25987e66aa252
Lake Developer <lakeinvestmentsprime@gmail.com>
2020-01-14 22:07:22 +0200

    start

---
+++ try.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int i;
+
+int main ()
+{
+  cout << "How old are you?" << endl;
+  cin >> i;
+
+  cout << "You are " << i << " years old!" << endl;
+  return 0;
+}
